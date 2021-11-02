4e36c432b1986d98873cfcd3ef22f501410bdd34
Willem <81077@rocteraa-student.nl>
2021-11-02 13:49:50 +0100

    File structure

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Kletsknop - Home</title>
 </head>
 <body>
-    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas earum tempore architecto minus reiciendis voluptatum eaque iure maxime modi voluptate temporibus quae, praesentium consectetur saepe dolorem. Sit saepe harum pariatur.</p>
 </body>
 </html>
