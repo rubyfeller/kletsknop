738014e96c15c5cb35d064474bf3996683f23b02
Ruby <ruby.feller@student.fontys.nl>
2021-11-03 10:25:58 +0100

    Added notification

---
+++ includes/navigation.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="buurtbewoners.php">Buurtbewoners</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul> 
+    </nav>
+</header>
---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+let buttonClicked = 0;
+if (buttonClicked == 0) {
+    showNotification();
+}
+function showNotification() {
+  document.getElementById("notification").style.display = "inline";
+}
