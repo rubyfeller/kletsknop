589665559a1af8f07e178fc6f4b00ca93190d62e
Willem <81077@rocteraa-student.nl>
2021-11-08 14:20:48 +0100

    Footer, Index, Navigatie, Contact

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class="footerLogo"><a href="index.php"><img class= "logo" src="img/kletsknopLogo.png"></a></div>
+    <div class="footerLogo"><a href="index.php"><img class= "kletsknop" src="img/kletsknop.png"></a></div>
+    <div class="contactInfo">
+        <ul class="contactInfoTop">
+            <li>Jassenstraat 27 <br>5722 AM Eindhoven</li>
+            <li>031 06 12345678 <br>info@fontys.nl</li>
+            <li>Disclaimer <br>Privacy</li>
+            <li>©2021 <br>Cookies</li>
+        </ul>
+    </div>
+    <div class="socialMedia">
+        <img class="smLogo" src="img/socialmedia.png">
+    </div>
+</footer>
--- registreren.php
+++ registreren.php
@@ -5,11 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.min.css">
-    <title>Kletsknop - BuurtBewoners</title>
+    <title>Kletsknop - Registreren</title>
 </head>
 <?php include "includes/navigation.php"?>
 <body>
-    <p>BuurtBewoners</p>
+    <p>Registreren</p>
 </body>
-<?php include "includes/footer.php"?>
 </html>
