b6796660778213dcb839f247295c60f6470a13df
Willem <81077@rocteraa-student.nl>
2021-11-11 10:02:13 +0100

    final changes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let counter = 0;
 let counterElem = document.querySelector(".peopleUnderway");
 let peopleUnderwayStored = localStorage.getItem("peopleUnderway");
 
+setInterval(getMessage, 5000);
+
 if (buttonClicked == 0) {
   showNotification();
 }
@@ -16,6 +18,28 @@ if (!localStorage.getItem("peopleUnderway")) {
 function showNotification() {
   document.getElementById("notification").style.display = "inline";
 }
+
+async function getMessage() {
+  fetch('http://192.168.48.42:8080/API/clicks')
+  .then(data => {
+  return data.json();
+  })
+  .then(post => {
+    if (post.msg.clickMessage) {
+      console.log(post.msg.clickMessage);
+      let clickId = post.msg.clickId;
+      let clickMessage = post.msg.clickMessage;
+      let clickTime = post.msg.clickTime;
+      document.getElementById("serverMessage").innerHTML = clickId + " " +  clickMessage + " " + clickTime;
+      document.getElementById("notificationData").innerHTML = "Er is iemand eenzaam";
+    }
+    else {
+      console.log("Er is nog niet op de knop gedrukt");
+    }
+  
+  });
+}
+
 
 function accept() {
   peopleUnderwayStored++;
@@ -43,22 +67,3 @@ function rejectNotification() {
   }, 3000);
 }
 
-async function getMessage() {
-  fetch('http://192.168.48.42:8080/API/clicks')
-  .then(data => {
-  return data.json();
-  })
-  .then(post => {
-  if (post.msg.clickMessage) {
-  console.log(post.msg.clickMessage);
-  let clickId = post.msg.clickId;
-  let clickMessage = post.msg.clickMessage;
-  let clickTime = post.msg.clickTime;
-  document.getElementById("serverMessage").innerHTML = clickId + " " +  clickMessage + " " + clickTime;
-  }
-  else {
-    console.log("Er is nog niet op de knop gedrukt");
-  }
-  
-});
-}
